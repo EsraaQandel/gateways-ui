dd68c25846aef171f5bf0cd1181d0b20b066e796
EsraaIbrahimQandeel <esraa.qandeel@devfactory.com>
2023-01-27 10:04:08 +0200

    fix: wired typo with regular expression
    reference: https://stackoverflow.com/questions/2630418/javascript-regex-returning-true-then-false-then-true-etc/2630538#2630538

--- src/app/config/ip-regex.ts
+++ src/app/config/ip-regex.ts
@@ -1,7 +1,7 @@
-export const ipAddressRegex = new RegExp(
-  /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi
-);
+export const ipAddressRegex =
+  /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/i;
 
 export function isValidIpAddress(ip: string): boolean {
-  return ipAddressRegex.test(ip);
+  const isValid = ipAddressRegex.test(ip);
+  return isValid;
 }
